1616df6ba24aa29b83122019d9aaa85036a6eefc
Mathias Oertel <e0e9cf7e@opayq.com>
2019-09-25 09:58:47 +0200

    - optimization in terms of duplicate code

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,3 +1,5 @@
+
+
 class FizzBuzz {
 
   def fizzBuzz(numbers: Seq[Int])(implicit rules: Seq[Int => String]): Seq[String] = for {
@@ -5,20 +7,28 @@ class FizzBuzz {
     step <- rules.map(rule => rule(index)).mkString :: Nil
   } yield if (step.isEmpty) index.toString else step
 
-  def inCaseOf[T](elem: T)(cond: T => Boolean)(put: String): String = if (cond(elem)) put else ""
+  def dividableBy(divisor: Int)(put: String): Int => String = {
+    num: Int => inCaseOf(num)(_ % divisor == 0)(put = put)
+  }
+
+  def digitIs(digitC: Char)(put: String): Int => String = {
+    num: Int => (num.toString map { case digit: Char => inCaseOf(digit)(_ == digitC)(put = put) }).mkString
+  }
+
+  private def inCaseOf[T](elem: T)(cond: T => Boolean)(put: => String): String = if (cond(elem)) put else ""
 
 }
 
 object FizzBuzz extends FizzBuzz {
 
   implicit val fizzBuzzRules: Seq[Int => String] = Seq(
-    { num => inCaseOf(num)(_ % 3 == 0)(put = "Fizz") },
-    { num => inCaseOf(num)(_ % 5 == 0)(put = "Buzz") },
+    dividableBy(3)(put = "Fizz"),
+    dividableBy(5)(put = "Buzz")
   )
 
   implicit val fizzBuzzRulesExtended: Seq[Int => String] = fizzBuzzRules ++ Seq(
-    { num => (num.toString map { case digit: Char => inCaseOf(digit)(_ == '3')(put = "Fizz") }).mkString },
-    { num => (num.toString map { case digit: Char => inCaseOf(digit)(_ == '5')(put = "Buzz") }).mkString },
+    digitIs('3')("Fizz"),
+    digitIs('5')("Buzz")
   )
 
 }
@@ -28,6 +38,5 @@ object Main extends App {
   import FizzBuzz.fizzBuzzRulesExtended
 
   print(FizzBuzz fizzBuzz (1 to 100))
-  print(FizzBuzz fizzBuzz (1 to 200))
 
 }
